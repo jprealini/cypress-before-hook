63986b9a01fcc9ff9f4f4e0ff10cce0fb8d91b78
Juan Pablo Realini <jprealini@gmail.com>
2024-04-22 23:09:43 -0300

    First Commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it.skip('passes 1', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('passes 2', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
